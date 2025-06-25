f151caf3ae7b525b4e4c54676f363be69f71614f
Markus Juhl Larsen <markusjuhllarsen@gmail.com>
2025-06-25 14:49:31 +0300

    Handling for n not being divisible by number of processes.

--- mpi/parallel-pi-general/parallel_pi.c
+++ mpi/parallel-pi-general/parallel_pi.c
@@ -15,18 +15,33 @@ int main(int argc, char** argv)
   int istart = n / ntasks * rank + 1;
   int istop = n / ntasks * rank + n / ntasks;
 
-   if (0 == rank) {
-      printf("Computing approximation to pi with N=%d\n", n);
-      printf("Using %d MPI processes\n", ntasks);
-   }
+  int remainder = n % ntasks;
+  // Adjust the start and stop indices if there are any remaining elements
+  // that need to be distributed among the processes.
+  // The first 'remainder' processes will get one extra element.
+  // The last 'ntasks - remainder' processes will get the same number of elements.
+  if (remainder > 0){
+    if (rank < remainder){
+      istart += rank;
+      istop += rank + 1;
+    } else {
+      istart += remainder;
+      istop += remainder;
+    }
+  }
+
+  if (0 == rank) {
+    printf("Computing approximation to pi with N=%d\n", n);
+    printf("Using %d MPI processes\n", ntasks);
+  }
   
-  if (rank == 0){
-    double local_pi = 0.0;
+  double local_pi = 0.0;
     for (int i=istart; i <= istop; i++) {
         double x = (i - 0.5) / n;
         local_pi += 1.0 / (1.0 + x*x);
-    }
+  }
 
+  if (rank == 0){
     double pi = local_pi;
     for (int i=1; i < ntasks; i++) {
         MPI_Recv(&local_pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -34,14 +49,7 @@ int main(int argc, char** argv)
     }
     pi *= 4.0 / n;
     printf("Approximate pi=%18.16f (exact pi=%10.8f)\n", pi, M_PI);
-  }
-  
-  if (rank != 0){
-    double local_pi = 0.0;
-    for (int i=istart; i <= istop; i++) {
-        double x = (i - 0.5) / n;
-        local_pi += 1.0 / (1.0 + x*x);
-    }
+  } else {
     MPI_Send(&local_pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
   }
 
