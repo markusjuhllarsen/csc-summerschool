9c6f466503f16dd7ed36dcc952b21064bad4aaee
Markus Juhl Larsen <markusjuhllarsen@gmail.com>
2025-06-27 10:26:23 +0300

    Conditional compilation

--- openmp/lib-funcs/hello.cpp
+++ openmp/lib-funcs/hello.cpp
@@ -7,14 +7,20 @@ int main()
     printf("Hello world!\n");
     #pragma omp parallel private(thread_id) shared(num_threads)
     {   
+        #ifdef _OPENMP
         thread_id = omp_get_thread_num();
 
         #pragma omp single
         // This block is executed by only one thread
         num_threads = omp_get_num_threads();
+        #else
+        thread_id = 1; // Fallback for non-OpenMP environments
+        num_threads = 1; // Fallback for non-OpenMP environments
+        #endif
 
         #pragma omp critical
         // This block is executed by only one thread at a time
+        // to not have interleaving output from different threads
         printf("Hello from thread %d\n", thread_id);
     }
     printf("There are %d threads in total.\n", num_threads);
