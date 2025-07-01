db383dbbfda62fb0bbdb201f44ed9abde57eb6be
Markus Juhl Larsen <markusjuhllarsen@gmail.com>
2025-07-01 13:34:01 +0300

    Name of buffer was wrong

--- gpu/exercises/fundamentals/08-sycl/03-axpy/axpy.cpp
+++ gpu/exercises/fundamentals/08-sycl/03-axpy/axpy.cpp
@@ -22,7 +22,7 @@ int main() {
     // Submit the kernel to the queue
     q.submit([&](handler& h) {
       // Create accessors if necessary
-      accessor x_acc{x_buf, h, read};
+      accessor x_acc{buf_x, h, read};
       accessor y_acc{buf_y, h, read_write};
 
       h.parallel_for(range<1>(N), [=](id<1> idx) {
